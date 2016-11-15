418fcab7ff02e69f0a357a7012cf9199b20e3922
srikanthkatta2207 <srikanthkatta2207@gmail.com>
2016-11-15 21:28:49 +0530

    [Srikanth] added script for to notify ci build regarding mocha tests

---
+++ versionContractTests.js
@@ -0,0 +1,17 @@
+var Mocha = require('mocha');
+var mocha = new Mocha({});
+mocha.addFile('./modules/metadataversion/CreateVersionTest.js');
+mocha.addFile('./modules/metadataversion/ErrorMessageTest.js');
+mocha.addFile('./modules/metadataversion/GetVersionHistoryTest.js');
+mocha.addFile('./modules/metadataversion/GetVersionTest.js');
+mocha.addFile('./modules/metadataversion/CreateVersionTest.js');
+mocha.addFile('./modules/metadataversion/GetVersionDetailsTest.js');
+mocha.addFile('./modules/metadataversion/GetVersionDataTest.js');
+mocha.addFile('./modules/metadataversion/GetVersionDatagzTest.js');
+mocha.addFile('./modules/metadataversion/MetadataVersionsUserAuthTest.js');
+mocha.addFile('./modules/metadataversion/UnsupportedCallsTest.js');
+mocha.run(function(failures){
+	process.on('exit', function () {
+		process.exit(failures);
+	});
+});
--- versioningContractTests.sh
+++ versioningContractTests.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
- mocha -b modules/metadataversion/ErrorMessageTest.js --timeout 20000
- mocha -b modules/metadataversion/GetVersionHistoryTest.js --timeout 20000
- mocha -b modules/metadataversion/GetVersionTest.js --timeout 20000
- mocha -b modules/metadataversion/CreateVersionTest.js --timeout 20000
- mocha -b modules/metadataversion/GetVersionDetailsTest.js --timeout 20000
- mocha -b modules/metadataversion/GetVersionDataTest.js --timeout 20000
- mocha -b modules/metadataversion/GetVersionDatagzTest.js --timeout 20000
- mocha -b modules/metadataversion/MetadataVersionsUserAuthTest.js --timeout 20000
- mocha -b modules/metadataversion/UnsupportedCallsTest.js --timeout 20000
+node versionContractTests.js
